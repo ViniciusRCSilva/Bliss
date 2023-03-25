d52ee4540aa89970484a69ee2f847fea89a6296e
Vinicius <viniciusflexa@gmail.com>
2023-03-25 10:38:51 -0300

    components style adjusts

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -6,12 +6,12 @@ interface InputProps{
 
 export default function Input(props: InputProps) {
     return (
-        <div className="flex w-full items-center rounded-lg border-[1px] bg-white border-green-blue shadow-md">
-            <div className="flex justify-center items-center w-[20%] text-2xl text-green-blue p-4 border-r-[1px] border-r-green-blue rounded-full">
+        <div className="flex w-full h-16 items-center rounded-lg border-[1px] px-4 bg-white border-green-blue shadow-md">
+            <div className="flex justify-center items-center w-[20%] text-3xl pr-4 text-green-blue border-r-[1px] border-r-green-blue">
                 {props.icon}
             </div>
 
-            <input type={props.type}  placeholder={props.placeholder} className="w-[80%] text-green-dark p-4 placeholder-gray focus:outline-none rounded-lg" />
+            <input type={props.type}  placeholder={props.placeholder} className="w-[80%] bg-white text-green-dark pl-4 placeholder-gray focus:outline-none rounded-lg" />
         </div>
     )
   }
--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -4,7 +4,7 @@ interface SelectProps{
 
 export function Select(props: SelectProps){
     return(
-        <select className="p-2 border-[1px] border-green-blue shadow-md rounded-lg focus:outline-none">
+        <select className="p-2 border-[1px] border-green-blue bg-white shadow-md rounded-lg focus:outline-none">
             {props.children}
         </select>
     )

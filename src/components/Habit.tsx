14b11a2ac644a8af4ac8ac6d934c67b665dd6780
Vinicius <viniciusflexa@gmail.com>
2023-03-28 21:03:08 -0300

    pop up info update

--- src/components/Habit.tsx
+++ src/components/Habit.tsx
@@ -3,45 +3,25 @@ import { Eye, NotePencil, Plus, X } from "@phosphor-icons/react";
 import Dialog from '@mui/material/Dialog';
 import DialogContent from '@mui/material/DialogContent';
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Select } from "./Select";
 
 const weekDays = [
-    'Dom',
-    'Seg',
-    'Ter',
-    'Qua',
-    'Qui',
-    'Sex',
-    'Sáb'
-]
-
-const Hour = [
-    '05:00',
-    '06:00',
-    '07:00',
-    '08:00',
-    '09:00',
-    '10:00',
-    '11:00',
-    '12:00',
-    '13:00',
-    '14:00',
-    '15:00',
-    '16:00',
-    '17:00',
-    '18:00',
-    '19:00',
-    '20:00',
-    '21:00',
-    '22:00',
-    '23:00',
+    { short: 'Dom', long: 'Domingo' },
+    { short: 'Seg', long: 'Segunda' },
+    { short: 'Ter', long: 'Terça' },
+    { short: 'Qua', long: 'Quarta' },
+    { short: 'Qui', long: 'Quinta' },
+    { short: 'Sex', long: 'Sexta' },
+    { short: 'Sáb', long: 'Sábado' },
 ]
 
 export function Habit(){
     const [open, setOpen] = useState(false);
+    const [day, setDay] = useState('');
 
-    const handleClickOpen = () => {
+    const handleClickOpen = (d: string) => {
+        setDay(d)
         setOpen(true);
     };
     
@@ -49,15 +29,24 @@ export function Habit(){
         setOpen(false);
     };
 
+    function GenerateHourList(HourNumber: number) {
+        let HourList = [];
+        for (let i = 5; i < HourNumber; i++) {
+          let hour = ("0" + i).slice(-2) + ":00";
+          HourList.push(hour);
+        }
+        return HourList;
+    }
+
     return(
         <>
             {weekDays.map((weekDay, i) => {
                 return(
                     <div className="flex w-full justify-between" key={`${weekDay}-${i}`}>
-                        <p className="text-3xl p-3">{weekDay}</p>
+                        <p className="text-2xl p-3">{weekDay.short}</p>
             
                         <div className="flex items-center justify-center border-2 border-green-blue rounded-lg gap-4 p-3">
-                            <div onClick={handleClickOpen} className="flex items-center gap-1 cursor-pointer">
+                            <div onClick={() => handleClickOpen(weekDay.long)} className="flex items-center gap-1 cursor-pointer">
                                 <Plus className="text-green-blue text-2xl" />
                                 <p className="text-xl">Criar</p>
                             </div>                           
@@ -86,11 +75,11 @@ export function Habit(){
                     <div className="flex flex-col justify-center items-start gap-4">
 
                         <div className="flex flex-col w-full gap-4">
-                            <p className="text-xl font-medium">Dia,</p>
+                            <p className="text-xl font-medium">{day},</p>
 
                             <p>meu novo hábito das:</p>
                             <Select>
-                                {Hour.map((hour, i) => {
+                                {GenerateHourList(24).map((hour, i) => {
                                     return(
                                         <option key={i} value={`${hour}`}>{hour}</option>
                                     )

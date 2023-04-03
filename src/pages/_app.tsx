7c02ecfa84032a8a2f2a066de811699e8385452b
Vinicius <viniciusflexa@gmail.com>
2023-04-03 15:13:18 -0300

    head title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,20 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Lexend } from 'next/font/google'
+import Head from 'next/head'
 
 const lexend = Lexend({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={lexend.className}>
-      <Component {...pageProps} />
-    </div>
+    <>    
+      <Head>
+        <title>Bliss - Bem-estar</title>
+      </Head>
+
+      <div className={lexend.className}>
+        <Component {...pageProps} />
+      </div>
+    </>
   )
 }

5c743a2ef45d3faa40a0a34a83866899921e28b7
Vinicius <viniciusflexa@gmail.com>
2023-04-05 15:16:31 -0300

    my account animation

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -12,9 +12,9 @@ export function Topbar(){
                         <Image src={Logo} alt="Logotipo do Bliss" width={80} />
                     </Link>
                     
-                    <Link href='/account' className='flex items-center gap-1'>
-                        <UserCircle className='text-4xl' weight='fill' />
-                        <p className='hidden lg:flex text-lg'>Minha conta</p>
+                    <Link href='/account' className='flex items-center group'>
+                        <UserCircle className='text-4xl transform lg:translate-x-2 lg:group-hover:translate-x-0 transition-transform' weight='fill' />
+                        <p className='hidden lg:flex text-lg transform opacity-0 group-hover:opacity-100 group-hover:translate-x-1 transition-all'>Minha conta</p>
                     </Link>
                 </div>
             </div>

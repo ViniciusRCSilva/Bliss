18f158607897616ca4ce8ae1ca7bc0b609872480
Vinicius <viniciusflexa@gmail.com>
2023-04-06 10:55:25 -0300

    date update (complete)

--- src/pages/diary.tsx
+++ src/pages/diary.tsx
@@ -7,11 +7,14 @@ import { useState } from "react";
 export default function Diary(){
     const date = new Date()
     const [day, setDay] = useState<string>(getDay(date.getDate()))
-    const [month, setMonth] = useState<any>(getMonth(date.getMonth()+1))
+    const [month, setMonth] = useState<string>(getMonth(date.getMonth()+1))
+    const [year, setYear] = useState<any>(date.getFullYear())
 
     let previousMonth = parseInt(month!)
 
-    let lastDayOfMonth = new Date(date.getFullYear(), previousMonth-1, 0).getDate()
+    let PreviousLastDayOfMonth = new Date(year, previousMonth-1, 0).getDate()
+
+    let AfterLastDayOfMonth = new Date(year, previousMonth, 0).getDate()
 
     function getDay(d: number){
         let day = ("0" + d).slice(-2)
@@ -32,14 +35,18 @@ export default function Diary(){
 
         let previousDay = today - 1
 
-        if(previousDay <= 0){
-            setDay(getDay(lastDayOfMonth))
+        console.log('hoje: '+ today +' | dia anterior: '+ previousDay +' | mês: '+ month +' | último dia do mês passado: '+ PreviousLastDayOfMonth)
+
+        if(previousDay < 1){
+            setDay(getDay(PreviousLastDayOfMonth))
             
             let previousMonth = actualMonth - 1
             setMonth(getMonth(previousMonth))
 
             if(previousMonth <= 0){
                 setMonth("12")
+
+                setYear(date.getFullYear()-1)
             }
         }else{
             setDay(getDay(previousDay))
@@ -55,9 +62,9 @@ export default function Diary(){
 
         let afterDay = today + 1
 
-        console.log('after day: '+ (afterDay-1) + ' last day of month: '+ lastDayOfMonth)
+        console.log('hoje: '+ today +' | dia depois: '+ afterDay +' | mês: '+ month +' | último dia desse mês: '+ AfterLastDayOfMonth)
 
-        if(afterDay-1 >= lastDayOfMonth){
+        if(afterDay > AfterLastDayOfMonth){
             setDay('01')
             
             let afterMonth = actualMonth + 1
@@ -65,6 +72,8 @@ export default function Diary(){
 
             if(afterMonth > 12){
                 setMonth("01")
+
+                setYear(date.getFullYear())
             }
         }else{
             setDay(getDay(afterDay))
@@ -83,10 +92,10 @@ export default function Diary(){
 
                         <div className="flex flex-col justify-center items-center select-none">
                             <p className="font-light">Dia {day}/{month}</p>
-                            <p className="text-lg">{date.getFullYear()}</p>
+                            <p className="text-lg">{year}</p>
                         </div>
 
-                        {day == getDay(date.getDate()) && month == getMonth(date.getMonth()+1) ? (
+                        {day == getDay(date.getDate()) && month == getMonth(date.getMonth()+1) && year == date.getFullYear() ? (
                             <div className="opacity-0 w-9 h-9">
                             </div>
                         ) : (
@@ -96,7 +105,22 @@ export default function Diary(){
                         )}
                     </div>
 
-                    <TextDiary />
+                    {day == getDay(date.getDate()) && month == getMonth(date.getMonth()+1) && year == date.getFullYear() ? (
+                        <div className="animate-screenOpacity">
+                            <TextDiary />
+                        </div>
+                    ): (
+                        <div className="overflow-y-auto select-none">
+                            <p className="text-sm lg:text-base font-light">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin varius mauris quis leo facilisis maximus. Sed aliquam mi elit, eu varius mi facilisis a. Aliquam erat volutpat. Sed in erat non tortor laoreet pharetra in eu diam. Etiam aliquet blandit aliquam. Mauris lobortis faucibus magna, facilisis finibus est egestas eu. Maecenas et blandit lacus.
+                                Vestibulum in aliquet velit, blandit rhoncus sapien. Curabitur a mauris lorem. Suspendisse vel quam ac elit sodales viverra. Curabitur rutrum nibh augue. Nam at viverra odio, vel molestie felis. Duis pellentesque, massa ac finibus euismod, ipsum libero bibendum ante, sed aliquet felis lectus quis massa. Etiam sagittis aliquet malesuada. Ut ut tellus velit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed mauris est, dictum quis dapibus malesuada, gravida et mi.
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin varius mauris quis leo facilisis maximus. Sed aliquam mi elit, eu varius mi facilisis a. Aliquam erat volutpat. Sed in erat non tortor laoreet pharetra in eu diam. Etiam aliquet blandit aliquam. Mauris lobortis faucibus magna, facilisis finibus est egestas eu. Maecenas et blandit lacus.
+                                Vestibulum in aliquet velit, blandit rhoncus sapien. Curabitur a mauris lorem. Suspendisse vel quam ac elit sodales viverra. Curabitur rutrum nibh augue. Nam at viverra odio, vel molestie felis. Duis pellentesque, massa ac finibus euismod, ipsum libero bibendum ante, sed aliquet felis lectus quis massa. Etiam sagittis aliquet malesuada. Ut ut tellus velit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed mauris est, dictum quis dapibus malesuada, gravida et mi.
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin varius mauris quis leo facilisis maximus. Sed aliquam mi elit, eu varius mi facilisis a. Aliquam erat volutpat. Sed in erat non tortor laoreet pharetra in eu diam. Etiam aliquet blandit aliquam. Mauris lobortis faucibus magna, facilisis finibus est egestas eu. Maecenas et blandit lacus.
+                                Vestibulum in aliquet velit, blandit rhoncus sapien. Curabitur a mauris lorem. Suspendisse vel quam ac elit sodales viverra. Curabitur rutrum nibh augue. Nam at viverra odio, vel molestie felis. Duis pellentesque, massa ac finibus euismod, ipsum libero bibendum ante, sed aliquet felis lectus quis massa. Etiam sagittis aliquet malesuada. Ut ut tellus velit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed mauris est, dictum quis dapibus malesuada, gravida et mi.
+                            </p>
+                        </div>
+                    )}
                 </div>
 
                 <Navigate back="Hábitos" linkBack="habits" next="Contatos de ajuda" linkNext="contacts" />

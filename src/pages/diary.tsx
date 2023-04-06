bfaf17142d89b5439082be9f4a4a306d14900fd5
Vinicius <viniciusflexa@gmail.com>
2023-04-05 23:22:42 -0300

    diary time update (in progress)

--- src/pages/diary.tsx
+++ src/pages/diary.tsx
@@ -2,14 +2,16 @@ import { Navigate } from "@/components/Navigate";
 import { TextDiary } from "@/components/TextDiary";
 import { Topbar } from "@/components/Topbar";
 import { CaretLeft, CaretRight } from "@phosphor-icons/react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function Diary(){
     const date = new Date()
-    const [day, setDay] = useState<string>()
-    const [month, setMonth] = useState<string>()
+    const [day, setDay] = useState<string>(getDay(date.getDate()))
+    const [month, setMonth] = useState<any>(getMonth(date.getMonth()+1))
 
-    let lastDayOfMonth = new Date(date.getFullYear(), date.getMonth()+1, 0).getDate()
+    let previousMonth = parseInt(month!)
+
+    let lastDayOfMonth = new Date(date.getFullYear(), previousMonth-1, 0).getDate()
 
     function getDay(d: number){
         let day = ("0" + d).slice(-2)
@@ -17,36 +19,57 @@ export default function Diary(){
     }
 
     function getMonth(m: number){
-        let month = ("0" + (m+1)).slice(-2)
+        let month = ("0" + m).slice(-2)
         return month
     }
 
-    function getPreviousDay(d: string){
+    function getPreviousDate(d: string, m: string){
         let day = d
+        let month = m
+
         let today = parseInt(day)
+        let actualMonth = parseInt(month)
+
         let previousDay = today - 1
+
         if(previousDay <= 0){
-            return setDay(getDay(lastDayOfMonth))
+            setDay(getDay(lastDayOfMonth))
+            
+            let previousMonth = actualMonth - 1
+            setMonth(getMonth(previousMonth))
+
+            if(previousMonth <= 0){
+                setMonth("12")
+            }
         }else{
-            return setDay(getDay(previousDay))
+            setDay(getDay(previousDay))
         }
     }
 
-    function getAfterDay(d: string){
+    function getAfterDate(d: string, m: string){
         let day = d
+        let month = m
+
         let today = parseInt(day)
+        let actualMonth = parseInt(month)
+
         let afterDay = today + 1
-        if(afterDay >= lastDayOfMonth){
-            return setDay('01')
+
+        console.log('after day: '+ (afterDay-1) + ' last day of month: '+ lastDayOfMonth)
+
+        if(afterDay-1 >= lastDayOfMonth){
+            setDay('01')
+            
+            let afterMonth = actualMonth + 1
+            setMonth(getMonth(afterMonth))
+
+            if(afterMonth > 12){
+                setMonth("01")
+            }
         }else{
-            return setDay(getDay(afterDay))
+            setDay(getDay(afterDay))
         }
     }
-
-    useEffect(() => {
-        setDay(getDay(date.getDate()))
-        setMonth(getMonth(date.getMonth()))
-    }, [])
 
     return(
         <div className="animate-screenOpacity">
@@ -54,26 +77,20 @@ export default function Diary(){
             <div className="flex flex-col w-full h-screen justify-evenly items-center pt-16 gap-5 bg-white">
                 <div className="flex flex-col w-[80%] h-[80%] gap-10">
                     <div className="flex w-full items-center justify-between">
-                        <div onClick={() => getPreviousDay(day!)} className="flex w-9 h-9 justify-center items-center text-white bg-green-blue rounded-full cursor-pointer">
+                        <div onClick={() => getPreviousDate(day, month)} className="flex w-9 h-9 justify-center items-center text-white bg-green-blue rounded-full cursor-pointer">
                             <CaretLeft weight="bold" className="text-xl"/>
                         </div>
 
                         <div className="flex flex-col justify-center items-center select-none">
-                            {/* <input 
-                                type="date"
-                                defaultValue={`${date.getFullYear()}-${getMonth(date.getMonth())}-${day}`}
-                                max={`${date.getFullYear()}-${getMonth(date.getMonth())}-${day}`}
-                                className="font-light border-2 border-green-blue p-2 rounded-md focus:outline-none"
-                            /> */}
-                            <p className="font-light">Dia {day}/{getMonth(date.getMonth())}</p>
+                            <p className="font-light">Dia {day}/{month}</p>
                             <p className="text-lg">{date.getFullYear()}</p>
                         </div>
 
-                        {day == getDay(date.getDate()) && month == getMonth(date.getMonth()) ? (
+                        {day == getDay(date.getDate()) && month == getMonth(date.getMonth()+1) ? (
                             <div className="opacity-0 w-9 h-9">
                             </div>
                         ) : (
-                            <div onClick={() => getAfterDay(day!)} className="flex animate-screenOpacity w-9 h-9 justify-center items-center text-white bg-green-blue rounded-full cursor-pointer">
+                            <div onClick={() => getAfterDate(day, month)} className="flex animate-screenOpacity w-9 h-9 justify-center items-center text-white bg-green-blue rounded-full cursor-pointer">
                                 <CaretRight weight="bold" className="text-xl"/>
                             </div>
                         )}

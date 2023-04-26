aa851fbd00a5a40acfaf2b1aa58c9c37ff4e5a86
Vinicius <viniciusflexa@gmail.com>
2023-04-26 14:37:41 -0300

    update

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -8,62 +8,60 @@ import LateralImage from '../../public/imagem_lateral_login.svg'
 
 export default function Login() {
     return (
-        <div className="fixed w-full h-[110vh] lg:h-screen">
-            <div className="flex flex-col lg:flex-row bg-[url('../../public/bottom_bliss.svg')] bg-no-repeat bg-bottom bg-contain sm:bg-none md:justify-center justify-start pt-[4vh] md:pt-0 items-center w-full h-full text-gray animate-screenOpacity">
-                <div className="lg:flex flex-col hidden text-white bg-green-blue w-[60%] h-full items-center justify-center gap-4 px-5">
-                    <Image src={LateralImage} alt="Image lateral" width={500}/>
-                    <p className="text-3xl font-medium">Seja bem-vindo(a)!</p>
-                    <p className="text-xl font-light text-center">Cuide da sua mente, corpo e alma com Bliss - sua jornada pessoal de bem-estar</p>
+        <div className="flex flex-col lg:flex-row bg-[url('../../public/bottom_bliss.svg')] bg-no-repeat bg-bottom bg-contain sm:bg-none md:justify-center justify-start pt-[4vh] md:pt-0 items-center w-full h-screen text-gray animate-screenOpacity">
+            <div className="lg:flex flex-col hidden text-white bg-green-blue w-[60%] h-full items-center justify-center gap-4 px-5">
+                <Image src={LateralImage} alt="Image lateral" width={500}/>
+                <p className="text-3xl font-medium">Seja bem-vindo(a)!</p>
+                <p className="text-xl font-light text-center">Cuide da sua mente, corpo e alma com Bliss - sua jornada pessoal de bem-estar</p>
+            </div>
+
+            <div className="flex flex-col items-center w-[80%] md:w-[40%] gap-5">
+                <div>
+                    <Image src={Logo} alt="Logotipo do Bliss" width={190} />
                 </div>
 
-                <div className="flex flex-col items-center w-[80%] md:w-[40%] gap-5">
-                    <div>
-                        <Image src={Logo} alt="Logotipo do Bliss" width={190} />
+                <form className="flex flex-col justify-center items-center gap-5">
+                    <p className="font-light text-xl">LOGIN</p>
+
+                    <div className="flex flex-col gap-5">
+                        <Input icon={<EnvelopeSimple/>} type="email" placeholder="Email" />
+                        
+                        <Input icon={<Password/>} type="password" placeholder="Senha" />
                     </div>
 
-                    <form className="flex flex-col justify-center items-center gap-5">
-                        <p className="font-light text-xl">LOGIN</p>
+                    <div className="flex w-full justify-between items-center">
+                        <Link href='/forgotPassword'>
+                            <p className="font-light text-sm underline">Esqueci minha senha</p>
+                        </Link>
 
-                        <div className="flex flex-col gap-5">
-                            <Input icon={<EnvelopeSimple/>} type="email" placeholder="Email" />
-                            
-                            <Input icon={<Password/>} type="password" placeholder="Senha" />
-                        </div>
+                        <Link href='/register'>
+                            <p className="font-light text-sm underline">Criar conta</p>
+                        </Link>
+                    </div>
 
-                        <div className="flex w-full justify-between items-center">
-                            <Link href='/forgotPassword'>
-                                <p className="font-light text-sm underline">Esqueci minha senha</p>
-                            </Link>
+                    <button className="bg-green-blue rounded-lg px-4 py-2 text-xl text-white shadow-md lg:transition-shadow lg:shadow-transparent lg:hover:shadow-md">ENTRAR</button>
 
-                            <Link href='/register'>
-                                <p className="font-light text-sm underline">Criar conta</p>
-                            </Link>
-                        </div>
+                    <div className="flex items-center w-full gap-2">
+                        <div className="w-[50%] h-[2px] bg-gray rounded-full opacity-30" />
+                        <p className="text-gray text-sm">ou</p>
+                        <div className="w-[50%] h-[2px] bg-gray rounded-full opacity-30" />
+                    </div>
 
-                        <button className="bg-green-blue rounded-lg px-4 py-2 text-xl text-white shadow-md lg:transition-shadow lg:shadow-transparent lg:hover:shadow-md">ENTRAR</button>
+                </form>
 
-                        <div className="flex items-center w-full gap-2">
-                            <div className="w-[50%] h-[2px] bg-gray rounded-full opacity-30" />
-                            <p className="text-gray text-sm">ou</p>
-                            <div className="w-[50%] h-[2px] bg-gray rounded-full opacity-30" />
-                        </div>
+                <div className="flex flex-col items-center gap-3">
+                    <p>Entrar com:</p>
 
-                    </form>
-
-                    <div className="flex flex-col items-center gap-3">
-                        <p>Entrar com:</p>
-
-                        <div className="flex gap-3">
-                            <button className="flex items-center gap-2 bg-white border-gray border-2 border-opacity-30 rounded-lg px-4 py-2 text-xl shadow-md lg:transition-shadow lg:shadow-transparent lg:hover:shadow-md">
-                                <GoogleLogo className="text-green-blue text-3xl"/>
-                            </button>
-                            <button className="flex items-center gap-2 bg-white border-gray border-2 border-opacity-30 rounded-lg px-4 py-2 text-xl shadow-md lg:transition-shadow lg:shadow-transparent lg:hover:shadow-md">
-                                <FacebookLogo className="text-green-blue text-3xl"/>
-                            </button>
-                            <button className="flex items-center gap-2 bg-white border-gray border-2 border-opacity-30 rounded-lg px-4 py-2 text-xl shadow-md lg:transition-shadow lg:shadow-transparent lg:hover:shadow-md">
-                                <AppleLogo className="text-green-blue text-3xl"/>
-                            </button>
-                        </div>
+                    <div className="flex gap-3">
+                        <button className="flex items-center gap-2 bg-white border-gray border-2 border-opacity-30 rounded-lg px-4 py-2 text-xl shadow-md lg:transition-shadow lg:shadow-transparent lg:hover:shadow-md">
+                            <GoogleLogo className="text-green-blue text-3xl"/>
+                        </button>
+                        <button className="flex items-center gap-2 bg-white border-gray border-2 border-opacity-30 rounded-lg px-4 py-2 text-xl shadow-md lg:transition-shadow lg:shadow-transparent lg:hover:shadow-md">
+                            <FacebookLogo className="text-green-blue text-3xl"/>
+                        </button>
+                        <button className="flex items-center gap-2 bg-white border-gray border-2 border-opacity-30 rounded-lg px-4 py-2 text-xl shadow-md lg:transition-shadow lg:shadow-transparent lg:hover:shadow-md">
+                            <AppleLogo className="text-green-blue text-3xl"/>
+                        </button>
                     </div>
                 </div>
             </div>
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -9,42 +9,40 @@ import LateralImage from '../../public/imagem_lateral_cadastro.svg'
 
 export default function Register() {
     return (
-        <div className="fixed w-full h-[110vh] lg:h-screen">
-            <div className="flex flex-col lg:flex-row bg-[url('../../public/bottom_bliss.svg')] bg-no-repeat bg-bottom bg-contain sm:bg-none md:justify-center justify-start pt-[4vh] md:pt-0 items-center w-full h-full text-gray animate-screenOpacity">
-                <div className="lg:flex flex-col hidden text-white bg-green-blue w-[60%] h-full items-center justify-center gap-4 px-5">
-                    <Image src={LateralImage} alt="Image lateral" width={500}/>
-                    <p className="text-3xl font-medium">Novo por aqui?</p>
-                    <p className="text-xl font-light text-center">Crie uma conta no Bliss para desbloquear uma experiência personalizada de bem-estar, projetada para ajudá-lo a alcançar seus objetivos de saúde mental e física.</p>
+        <div className="flex flex-col lg:flex-row bg-[url('../../public/bottom_bliss.svg')] bg-no-repeat bg-bottom bg-contain sm:bg-none md:justify-center justify-start pt-[4vh] md:pt-0 items-center w-full h-screen text-gray animate-screenOpacity">
+            <div className="lg:flex flex-col hidden text-white bg-green-blue w-[60%] h-full items-center justify-center gap-4 px-5">
+                <Image src={LateralImage} alt="Image lateral" width={500}/>
+                <p className="text-3xl font-medium">Novo por aqui?</p>
+                <p className="text-xl font-light text-center">Crie uma conta no Bliss para desbloquear uma experiência personalizada de bem-estar, projetada para ajudá-lo a alcançar seus objetivos de saúde mental e física.</p>
+            </div>
+            
+            <div className="flex flex-col justify-center items-center w-[80%] md:w-[40%] gap-10">
+                <div>
+                    <Image src={Logo} alt="Logotipo do Bliss" width={190} />
                 </div>
-                
-                <div className="flex flex-col justify-center items-center w-[80%] md:w-[40%] gap-10">
-                    <div>
-                        <Image src={Logo} alt="Logotipo do Bliss" width={190} />
-                    </div>
 
-                    <form className="flex flex-col justify-center items-center gap-5">
-                        <p className="font-light text-xl">CADASTRO</p>
+                <form className="flex flex-col justify-center items-center gap-5">
+                    <p className="font-light text-xl">CADASTRO</p>
 
-                        <div className="flex flex-col gap-5">
-                            <Input icon={<SmileyWink/>} type="text" placeholder="Nome" />
+                    <div className="flex flex-col gap-5">
+                        <Input icon={<SmileyWink/>} type="text" placeholder="Nome" />
 
-                            <Input icon={<EnvelopeSimple/>} type="email" placeholder="Email" />
-                            
-                            <div className="flex flex-col">
-                                <p>Data de nascimento</p>
-                                <Input icon={<Baby/>} type="date" />
-                            </div>
-
-                            <Input icon={<Password/>} type="password" placeholder="Senha" />
+                        <Input icon={<EnvelopeSimple/>} type="email" placeholder="Email" />
+                        
+                        <div className="flex flex-col">
+                            <p>Data de nascimento</p>
+                            <Input icon={<Baby/>} type="date" />
                         </div>
 
-                        <Link href='/login'>
-                            <p className="font-light text-sm underline">Já tenho uma conta!</p>
-                        </Link>
+                        <Input icon={<Password/>} type="password" placeholder="Senha" />
+                    </div>
 
-                        <div onClick={() => Router.push('/additionalInfo')} className="bg-green-blue rounded-lg px-4 py-2 text-xl text-white shadow-md lg:cursor-pointer lg:transition-shadow lg:shadow-transparent lg:hover:shadow-md">CADASTRAR</div>
-                    </form>
-                </div>
+                    <Link href='/login'>
+                        <p className="font-light text-sm underline">Já tenho uma conta!</p>
+                    </Link>
+
+                    <div onClick={() => Router.push('/additionalInfo')} className="bg-green-blue rounded-lg px-4 py-2 text-xl text-white shadow-md lg:cursor-pointer lg:transition-shadow lg:shadow-transparent lg:hover:shadow-md">CADASTRAR</div>
+                </form>
             </div>
         </div>
     )

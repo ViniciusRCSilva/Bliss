11eb2165dc97f8f3f1c64db7b9b789ca066dcea3
Vinicius <viniciusflexa@gmail.com>
2023-05-05 15:43:32 -0300

    padding fix

--- src/pages/habits.tsx
+++ src/pages/habits.tsx
@@ -6,7 +6,7 @@ export default function Habits(){
     return(
         <div className="animate-screenOpacity">
             <Topbar active="habits" />
-            <div className="flex flex-col w-full h-screen items-center pt-16 lg:pt-0 bg-white">
+            <div className="flex flex-col w-full h-screen items-center bg-white">
                 <div className="flex flex-col w-[80%] h-full justify-center gap-5">
                     <p className="text-xl">Meus hábitos</p>
 

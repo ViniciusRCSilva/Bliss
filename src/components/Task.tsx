c852c9499922ef836e2cb31f4f385f89b6fe3f72
Vinicius <viniciusflexa@gmail.com>
2023-05-14 23:49:27 -0300

    style update

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -27,16 +27,12 @@ export function Task(props: TaskProps){
 
             {props.edit === true ? (
                 <>
-                    <div onClick={handleOpacity} className={`hidden w-9 h-9 justify-center items-center ${opacity} bg-green-blue  lg:hover:opacity-100 transition-opacity cursor-pointer text-white rounded-lg`}>
-                        <Check weight="bold" />
-                    </div>
-
                     <div className="flex items-center gap-4">
-                        <div className={`flex w-9 h-9 justify-center items-center bg-yellow-300 cursor-pointer text-white rounded-lg`}>
+                        <div className={`flex w-9 h-9 justify-center items-center bg-yellow-300 cursor-pointer text-white rounded-lg lg:hover:opacity-90 transition-opacity`}>
                             <PencilSimple weight="bold" />
                         </div>
 
-                        <div className={`flex w-9 h-9 justify-center items-center bg-red-600 cursor-pointer text-white rounded-lg`}>
+                        <div className={`flex w-9 h-9 justify-center items-center bg-red-500 cursor-pointer text-white rounded-lg lg:hover:opacity-90 transition-opacity`}>
                             <TrashSimple weight="bold" />
                         </div>
                     </div>
@@ -45,16 +41,6 @@ export function Task(props: TaskProps){
                 <>
                     <div onClick={handleOpacity} className={`flex w-9 h-9 justify-center items-center ${opacity} bg-green-blue  lg:hover:opacity-100 transition-opacity cursor-pointer text-white rounded-lg`}>
                         <Check weight="bold" />
-                    </div>
-
-                    <div className="hidden items-center gap-4">
-                        <div className={`flex w-9 h-9 justify-center items-center bg-yellow-300 cursor-pointer text-white rounded-lg`}>
-                            <PencilSimple weight="bold" />
-                        </div>
-
-                        <div className={`hidden w-9 h-9 justify-center items-center bg-red-600 cursor-pointer text-white rounded-lg`}>
-                            <TrashSimple weight="bold" />
-                        </div>
                     </div>
                 </>                
             )}

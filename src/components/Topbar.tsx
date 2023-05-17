fa9814c7f154c771d54e41ecc91d4a2369251472
Vinicius <viniciusflexa@gmail.com>
2023-05-17 10:35:35 -0300

    topbar update

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -11,70 +11,72 @@ export function Topbar({ active }: TopbarProps){
     return(
         <div className="fixed w-full h-16 bg-white shadow-md">
             <div className='flex w-full h-full items-center justify-center'>
-                <div className='flex w-[80%] items-center justify-between text-green-blue'>
+                <div className='flex w-[80%] lg:w-[95%] items-center justify-between text-green-blue'>
                     <Link href='/'>
                         <Image src={Logo} alt="Logotipo do Bliss" width={80} />
                     </Link>
 
-                    {active == 'house' ? (
-                        <Link href='/' className='hidden lg:flex items-center group'>
-                            <House className='text-4xl transform lg:translate-x-2 lg:group-hover:translate-x-0 transition-transform' weight='fill' />
-                            <p className='hidden lg:flex text-lg transform opacity-0 group-hover:opacity-100 group-hover:translate-x-1 transition-all'>Início</p>
-                        </Link>
-                    ) : (
-                        <Link href='/' className='hidden lg:flex items-center group'>
-                            <House className='text-4xl transform lg:translate-x-2 lg:group-hover:translate-x-0 transition-transform' />
-                            <p className='hidden lg:flex text-lg transform opacity-0 group-hover:opacity-100 group-hover:translate-x-1 transition-all'>Início</p>
-                        </Link>
-                    )}
+                    <div className='flex items-center gap-6'>
+                        {active == 'house' ? (
+                            <Link href='/' className='hidden lg:flex items-end gap-1'>
+                                <House className='text-3xl' weight='fill' />
+                                <p className='hidden lg:flex text-lg'>Início</p>
+                            </Link>
+                        ) : (
+                            <Link href='/' className='hidden lg:flex items-end gap-1 hover:opacity-60 transition-opacity'>
+                                <House className='text-3xl' />
+                                <p className='hidden lg:flex text-lg font-light'>Início</p>
+                            </Link>
+                        )}
 
-                    {active == 'habits' ? (
-                        <Link href='/habits' className='hidden lg:flex items-center group'>
-                            <ArrowsClockwise className='text-4xl transform lg:translate-x-2 lg:group-hover:translate-x-0 transition-transform' weight='fill' />
-                            <p className='hidden lg:flex text-lg transform opacity-0 group-hover:opacity-100 group-hover:translate-x-1 transition-all'>Hábitos</p>
-                        </Link>
-                    ) : (
-                        <Link href='/habits' className='hidden lg:flex items-center group'>
-                            <ArrowsClockwise className='text-4xl transform lg:translate-x-2 lg:group-hover:translate-x-0 transition-transform' />
-                            <p className='hidden lg:flex text-lg transform opacity-0 group-hover:opacity-100 group-hover:translate-x-1 transition-all'>Hábitos</p>
-                        </Link>
-                    )}
+                        {active == 'habits' ? (
+                            <Link href='/habits' className='hidden lg:flex items-end gap-1'>
+                                <ArrowsClockwise className='text-3xl' weight='fill' />
+                                <p className='hidden lg:flex text-lg'>Hábitos</p>
+                            </Link>
+                        ) : (
+                            <Link href='/habits' className='hidden lg:flex items-end gap-1 hover:opacity-60 transition-opacity'>
+                                <ArrowsClockwise className='text-3xl' />
+                                <p className='hidden lg:flex text-lg font-light'>Hábitos</p>
+                            </Link>
+                        )}
 
-                    {active == 'diary' ? (
-                        <Link href='/diary' className='hidden lg:flex items-center group'>
-                            <BookBookmark className='text-4xl transform lg:translate-x-2 lg:group-hover:translate-x-0 transition-transform' weight='fill' />
-                            <p className='hidden lg:flex text-lg transform opacity-0 group-hover:opacity-100 group-hover:translate-x-1 transition-all'>Diário</p>
-                        </Link>
-                    ) : (
-                        <Link href='/diary' className='hidden lg:flex items-center group'>
-                            <BookBookmark className='text-4xl transform lg:translate-x-2 lg:group-hover:translate-x-0 transition-transform' />
-                            <p className='hidden lg:flex text-lg transform opacity-0 group-hover:opacity-100 group-hover:translate-x-1 transition-all'>Diário</p>
-                        </Link>
-                    )}
+                        {active == 'diary' ? (
+                            <Link href='/diary' className='hidden lg:flex items-end gap-1'>
+                                <BookBookmark className='text-3xl' weight='fill' />
+                                <p className='hidden lg:flex text-lg'>Diário</p>
+                            </Link>
+                        ) : (
+                            <Link href='/diary' className='hidden lg:flex items-end gap-1 hover:opacity-60 transition-opacity'>
+                                <BookBookmark className='text-3xl' />
+                                <p className='hidden lg:flex text-lg font-light'>Diário</p>
+                            </Link>
+                        )}
 
-                    {active == 'contacts' ? (
-                        <Link href='/contacts' className='hidden lg:flex items-center group'>
-                            <Phone className='text-4xl transform lg:translate-x-2 lg:group-hover:translate-x-0 transition-transform' weight='fill' />
-                            <p className='hidden lg:flex text-lg transform opacity-0 group-hover:opacity-100 group-hover:translate-x-1 transition-all'>Contatos</p>
-                        </Link>
-                    ) : (
-                        <Link href='/contacts' className='hidden lg:flex items-center group'>
-                            <Phone className='text-4xl transform lg:translate-x-2 lg:group-hover:translate-x-0 transition-transform' />
-                            <p className='hidden lg:flex text-lg transform opacity-0 group-hover:opacity-100 group-hover:translate-x-1 transition-all'>Contatos</p>
-                        </Link>
-                    )}
+                        {active == 'contacts' ? (
+                            <Link href='/contacts' className='hidden lg:flex items-end gap-1'>
+                                <Phone className='text-3xl' weight='fill' />
+                                <p className='hidden lg:flex text-lg'>Contatos</p>
+                            </Link>
+                        ) : (
+                            <Link href='/contacts' className='hidden lg:flex items-end gap-1 hover:opacity-60 transition-opacity'>
+                                <Phone className='text-3xl' />
+                                <p className='hidden lg:flex text-lg font-light'>Contatos</p>
+                            </Link>
+                        )}
 
-                    {active == 'account' ? (
-                        <Link href='/account' className='flex items-center group'>
-                            <UserCircle className='text-4xl transform lg:translate-x-2 lg:group-hover:translate-x-0 transition-transform' weight='fill' />
-                            <p className='hidden lg:flex text-lg transform opacity-0 group-hover:opacity-100 group-hover:translate-x-1 transition-all'>Minha conta</p>
-                        </Link>
-                    ) : (
-                        <Link href='/account' className='flex items-center group'>
-                            <UserCircle className='text-4xl transform lg:translate-x-2 lg:group-hover:translate-x-0 transition-transform' />
-                            <p className='hidden lg:flex text-lg transform opacity-0 group-hover:opacity-100 group-hover:translate-x-1 transition-all'>Minha conta</p>
-                        </Link>
-                    )}
+                        {active == 'account' ? (
+                            <Link href='/account' className='flex items-end gap-1'>
+                                <UserCircle className='text-4xl lg:text-3xl' weight='fill' />
+                                <p className='hidden lg:flex text-lg'>Minha conta</p>
+                            </Link>
+                        ) : (
+                            <Link href='/account' className='flex items-end gap-1 hover:opacity-60 transition-opacity'>
+                                <UserCircle className='text-4xl lg:text-3xl' />
+                                <p className='hidden lg:flex text-lg font-light'>Minha conta</p>
+                            </Link>
+                        )}
+                    </div>
                 </div>
             </div>
         </div>

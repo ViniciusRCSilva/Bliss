417b026ff858507ac0b5ec98009e711f8b0dbee4
Vinicius <viniciusflexa@gmail.com>
2023-07-01 11:52:38 -0300

    new animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,11 +28,22 @@ module.exports = {
         to: {
           transform: 'rotate(360deg)'
         }
-      }
+      },
+      bounce: {
+        '0%, 100%': {
+          transform: 'translateY(-25%)',
+          animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)',
+        },
+        '50%': {
+          transform: 'translateY(0)',
+          animationTimingFunction: 'cubic-bezier(0, 0, 0.2, 1)',
+        }
+      },
     },
     animation: {
       screenOpacity: 'screenOpacity 0.5s ease-in-out',
       spin: 'spin 1s linear infinite',
+      bounce: 'bounce 1s infinite',
     }
   },
   plugins: [
